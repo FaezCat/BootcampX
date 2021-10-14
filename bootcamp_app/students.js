bcabbec2b9ca7944bd58f734e9b5bcba8e3b7ce5
faez <faezc@hotmail.com>
2021-10-14 04:55:38 +0000

    Refactored students.js and teachers.js files

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,14 +9,15 @@ const pool = new Pool({
 
 const cohort = process.argv[2];
 const limit = process.argv[3];
+const values = [`%${cohort}%`, limit];
 
 pool.query(`
 SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${cohort}%'
-LIMIT ${limit};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,7 +7,7 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const cohort = process.argv[2];
+const cohort = [process.argv[2]];
 
 pool.query(`
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
@@ -15,9 +15,9 @@ FROM assistance_requests
 JOIN teachers ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${cohort}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`, cohort)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
